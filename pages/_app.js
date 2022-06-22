9130cb7f0345f3aa05e83205d868c005359ccc38
muradrahmanzada <muradrahmanzada@gmail.com>
2022-06-22 14:24:14 +0400

    finished build

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/globals.css";
+import { Layout } from "../components";
+import { StateContext, stateContext } from "../context/StateContext";
+import {Toaster} from 'react-hot-toast';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
+}
+
+export default MyApp;
